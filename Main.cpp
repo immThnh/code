fc1c289649650d9f0854ee2eda01d78c437470ce
immThnh <103478846+immThnh@users.noreply.github.com>
2022-05-04 23:52:11 +0700

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+#include "PhanSo.cpp"
+using namespace std;
+
+int main()
+{
+    PhanSo a;
+    cin >> a;
+    
+    PhanSo b(a);
+    cout <<"PHAN SO B: " <<  b;
+
+
+    return 0;
+}
